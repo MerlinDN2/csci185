d45879db6bbb8f06458024e976e2cc2cede4aab5
Merlin <merlindn40@gmail.com>
2024-04-22 12:45:32 -0400

    uploading tutorial 11

---
+++ tutorials/tutorial11/exercise01/main.js
@@ -0,0 +1,28 @@
+function makeRed() {
+    // your code here...
+    console.log('Change the box\'s background color to red');
+    document.querySelector("#box").style.backgroundColor = "Red";
+    // console.log(#btn1, makeRed);
+}
+
+function makeBlue() {
+    // your code here...
+    console.log('Change the box\'s background color to blue');
+    document.querySelector("#box").style.backgroundColor = "Blue";
+    // console.log(#btn2, makeBlue);
+}
+
+function makePink() {
+    // your code here...
+    console.log('Change the box\'s background color to pink');
+    document.querySelector("#box").style.backgroundColor = "Pink";
+    // console.log(#btn3, makePink);
+}
+
+function makeOrange() {
+    // your code here...
+    console.log('Change the box\'s background color to orange');
+    document.querySelector("#box").style.backgroundColor = "Orange";
+    // console.log(btn#4, makeOrange);
+}
+
